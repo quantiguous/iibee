d1188a9adcd03cda68a3122d55d9fae875535198
akil <kataria.subs@gmail.com>
2015-09-11 06:47:52 +0530

    refactored code, and added support for http basic auth

---
+++ lib/iibee/connection.rb
@@ -0,0 +1,18 @@
+require 'faraday'
+
+module Iibee
+  class Connection
+    def initialize(options: {})
+      url = "#{options[:scheme]}://#{options[:host]}:#{options[:port]}".chomp(":")
+      @conn = Faraday.new(url) do |f|
+        f.response :logger
+        f.adapter :net_http_persistent
+      end
+      @conn.basic_auth(options[:user], options[:password]) unless options[:user].nil? or options[:password].nil?
+    end
+    
+    def get(url)
+      @conn.get(url)
+    end
+  end
+end
