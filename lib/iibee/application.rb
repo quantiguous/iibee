285922ab05535a45e5a5a0a17b4d2a1511f54563
divyajayan <divya.jayan@quantiguous.com>
2015-09-12 18:59:56 +0530

    Revert "Bump to 0.1.14"
    This reverts commit 6bc8a04e169ea840cdc14eacdd8b944df712b7a2.

--- lib/iibee/application.rb
+++ lib/iibee/application.rb
@@ -4,65 +4,45 @@ require 'oga'
 module Iibee
   class Application
     class Properties
-      attr_reader :version, :label, :runMode, :uuid, :isRunning, :shortDesc, :longDesc, :processId, :traceLevel, :soapNodesUseEmbeddedListener, 
-                  :compiledXPathCacheSizeEntries, :consoleMode, :httpNodesUseEmbeddedListener, :inactiveUserExitList, :activeUserExitList, 
-                  :traceNodeLevel, :userTraceLevel, :modifyTime, :deployTime, :barFileName
+      attr_reader :processId, :traceLevel, :soapNodesUseEmbeddedListener, :compiledXPathCacheSizeEntries, :consoleMode, :httpNodesUseEmbeddedListener, :inactiveUserExitList, :activeUserExitList, :traceNodeLevel, :userTraceLevel
       def initialize(document)
-        document.xpath('properties/*/property').each do |property|
+        document.xpath('properties/advancedProperties/property').each do |advancedProperty|
           
-          propertyName = property.get('name')
-          propertyValue = property.get('value')
+          propertyName = advancedProperty.get('name')
+          propertyValue = advancedProperty.get('value')
           instance_variable_set("@#{propertyName}", propertyValue)
-        end     
+        end
       end
     end
     CONTEXT_PATH = "apiv1/executiongroups"
     
-    attr_reader :type, :isRunning, :runMode, :startMode, :hasChildren, :uuid, :name, :propertiesUri, :executionGroupName, :options
+    attr_reader :type, :isRunning, :runMode, :startMode, :hasChildren, :uuid, :name, :propertiesUri
     
-    def initialize(document, executionGroupName, options)
-      document.xpath('@*').each do |attribute|        
+    def initialize(document)
+      document.xpath('application/@*').each do |attribute|        
         instance_variable_set("@#{attribute.name}", attribute.value)
       end
-      @executionGroupName = executionGroupName
-      @options = options
-    end
-    
-    def executionGroup
-      Iibee::ExecutionGroup.find_by(name: executionGroupName, options: @options)
     end
     
     def properties
-      url = "#{options[:scheme]}://#{options[:host]}:#{options[:port]}".chomp(":")
-      response = Faraday.get("#{url}/#{self.propertiesUri}")
+      response = Faraday.get("#{Iibee.configuration.base_url}/#{self.propertiesUri}")
       document = Oga.parse_xml(response.body)
       @properties = Iibee::Application::Properties.new(document)
     end
     
-    def self.find_by(executionGroupName: nil, name: nil, options: {})
-      where(executionGroupName: executionGroupName, name: name, options: options).first
-    end
-    
-    def self.where(executionGroupName: nil, name: nil, options: {}) 
+    def self.all(egName)
       applications = []
-
-      url = "#{options[:scheme]}://#{options[:host]}:#{options[:port]}".chomp(":")
-      
-      unless executionGroupName.nil?
-        application_url = "/#{CONTEXT_PATH}/#{executionGroupName}/?depth=2"
-      else
-        application_url = "/#{CONTEXT_PATH}/?depth=3"
-      end
-      
-      response = Faraday.get(url+application_url)
+      response = Faraday.get("#{Iibee.configuration.base_url}/#{CONTEXT_PATH}/#{egName}/applications/")
       document = Oga.parse_xml(response.body)
-      
-      document.xpath("//application[@name='#{name}']").each do |application|
-        applications << new(application, application.parent.parent.get('name'), options)
+      document.xpath('applications/application').each do |application|
+          applications << new(document)  
       end
-      
-      return applications
     end
     
+    def self.find_by_name(egName, name)
+      response = Faraday.get("#{Iibee.configuration.base_url}/#{CONTEXT_PATH}/#{egName}/applications/#{name}")
+      document = Oga.parse_xml(response.body)
+      new(document)        
+    end
   end
 end
--- test/test_service.rb
+++ test/test_service.rb
@@ -4,9 +4,9 @@ class TestService < Minitest::Test
 
   def test_it_gives_back_specific_service
     VCR.use_cassette('TestService_test_it_gives_back_specific_service') do
-      service = Iibee::Service.find_by(executionGroupName: 'aml', name: 'test', options: @@broker_spec)
+      service = Iibee::Service.find_by(executionGroupName: 'aml', name: 'SocialBankingService', options: @@broker_spec)
       assert_equal Iibee::Service, service.class
-      assert_equal "test", service.name
+      assert_equal "SocialBankingService", service.name
       assert_equal "aml", service.executionGroupName
       assert_equal "aml", service.executionGroup.name
     end
@@ -14,20 +14,20 @@ class TestService < Minitest::Test
 
   def test_it_gives_back_one_instance_of_the_service
     VCR.use_cassette('TestService_test_it_gives_back_one_instance_of_the_service') do
-      service = Iibee::Service.find_by(executionGroupName: 'aml', name: 'test', options: @@broker_spec)
+      service = Iibee::Service.find_by(executionGroupName: 'aml', name: 'SocialBankingService', options: @@broker_spec)
       assert_equal Iibee::Service, service.class
-      assert_equal "test", service.name
+      assert_equal "SocialBankingService", service.name
       assert_equal "aml", service.executionGroupName
     end
   end
 
   def test_it_gives_back_all_instances_of_a_service
     VCR.use_cassette('TestService_test_it_gives_back_all_instances_of_a_service') do
-      services = Iibee::Service.where(name: 'test', options: @@broker_spec)
+      services = Iibee::Service.where(name: 'SocialBankingService', options: @@broker_spec)
       executionGroups = ["Mock", "aml"]
       services.each do |service|
         assert_equal Iibee::Service, service.class
-        assert_equal "test", service.name
+        assert_equal "SocialBankingService", service.name
         assert_includes executionGroups, service.executionGroupName
       end
       assert_equal executionGroups.count, services.count
