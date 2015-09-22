b9c4a3887d76504081ae48433a1035eae098fe72
divyajayan <divya.jayan@quantiguous.com>
2015-09-22 17:24:27 +0530

    Bump to 0.1.16

---
+++ lib/iibee/message_flow.rb
@@ -0,0 +1,62 @@
+require 'oga'
+
+module Iibee
+  class MessageFlow
+    class Properties
+      attr_reader :version, :label, :runMode, :uuid, :isRunning, :commitCount, :traceLevel, :additionalInstances, :startMode,
+                  :coordinatedTransaction, :commitInterval, :userTraceLevel, :modifyTime, :deployTime, :barFileName
+      def initialize(document)        
+        document.xpath('properties/*/property').each do |property|
+          
+          propertyName = property.get('name')
+          propertyValue = property.get('value')
+          instance_variable_set("@#{propertyName}", propertyValue)
+        end 
+      end
+    end
+    CONTEXT_PATH = "apiv1/executiongroups"
+    
+    attr_reader :type, :isRunning, :runMode, :startMode, :hasChildren, :uuid, :name, :propertiesUri, :executionGroupName, :options
+    
+    def initialize(document, executionGroupName, options)
+      document.xpath('@*').each do |attribute|        
+        instance_variable_set("@#{attribute.name}", attribute.value)
+      end
+      @executionGroupName = executionGroupName
+      @options = options
+    end
+    
+    def executionGroup
+      Iibee::ExecutionGroup.find_by(name: executionGroupName, options: @options)
+    end
+    
+    def properties
+      response = Iibee::Connection.new(options: options).get("#{self.propertiesUri}")
+      document = Oga.parse_xml(response.body)
+      @properties = Iibee::Service::Properties.new(document)
+    end
+    
+    def self.find_by(executionGroupName: nil, name: nil, options: {})
+      where(executionGroupName: executionGroupName, name: name, options: options).first
+    end
+    
+    def self.where(executionGroupName: nil, name: nil, options: {}) 
+      msg_flows = []
+      
+      unless executionGroupName.nil?
+        msg_flow_url = "/#{CONTEXT_PATH}/#{executionGroupName}/?depth=4"
+      else
+        msg_flow_url = "/#{CONTEXT_PATH}/?depth=5"
+      end
+      
+      response = Iibee::Connection.new(options: options).get(msg_flow_url)
+      document = Oga.parse_xml(response.body)
+      
+      document.xpath("//messageflow[@name='#{name}']").each do |msg_flow|
+        msg_flows << new(msg_flow, msg_flow.parent.parent.get('name'), options)
+      end
+      
+      return msg_flows
+    end
+  end
+end
--- lib/iibee/version.rb
+++ lib/iibee/version.rb
@@ -1,3 +1,3 @@
 module Iibee
-  VERSION = "0.1.15"
+  VERSION = "0.1.16"
 end
